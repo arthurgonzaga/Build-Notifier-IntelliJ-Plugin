3efc7762505a8ea14cd4d0be5a6944119dfd7950
GitHub Action <action@github.com>
2022-12-23 19:42:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/arthurgonzaga/buildnotifierplugin/MyBundle.kt
+++ src/main/kotlin/com/github/arthurgonzaga/buildnotifierplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.arthurgonzaga.buildnotifierplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/arthurgonzaga/buildnotifierplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arthurgonzaga/buildnotifierplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.arthurgonzaga.buildnotifierplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arthurgonzaga.buildnotifierplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
